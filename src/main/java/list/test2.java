000ae72253df189f011668daa249a8ea46e24f08
陈汶宁 <chenwenning@koolearn.com>
2017-08-02 18:42:56 +0800

    提交

--- src/main/java/list/test2.java
+++ src/main/java/list/test2.java
@@ -5,4 +5,36 @@ package list;
  */
 public class test2 {
 
+    public static void main(String[] args) {
+        int n = 100;
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= n; i = i + 2) {
+            if (i > 3) {
+                if (isPrimeNumber(i)) {
+                    System.out.println(i);
+                }
+            }
+
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start));
+    }
+
+    public static boolean isPrimeNumber(int a) {
+        boolean flag = true;
+        System.out.println("init :"+ a);
+        int temp = (int) Math.sqrt(a);
+        System.out.println("sqrt :"+ temp);
+        if (a % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= temp; i = i + 2) {
+            if (a % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
 }
